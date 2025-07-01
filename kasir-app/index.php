07ebcb472aecf8bb83d2669e5ce880c6e3bfd222
kayyy <2306064@itg.ac.id>
2025-07-01 21:21:51 +0700

    Delete kasir-app directory

--- kasir-app/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['id_kasir'])) {
-    header("Location: modules/akun/login.php");
-    exit;
-} else {
-    header("Location: modules/home.php"); // atau index dashboard kamu
-    exit;
-}
